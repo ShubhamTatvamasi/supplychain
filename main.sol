76fbe229583d00a4355d0de31f40d3b206b8cc8b
Shubham Tatvamasi <shubhamtatvamasi@gmail.com>
2018-04-12 12:51:45 +0530

    Added newActor function

--- main.sol
+++ main.sol
@@ -9,7 +9,16 @@ contract SupplyChain {
         string planet;
     }
 
+    struct Actor {
+
+        string companyName;
+        string contactInfo;
+        string products;
+    }
+
     Survey[] public surveys;
+    Actor[] public actors;
+    
 
     function SupplyChain() public {
         
@@ -26,6 +35,19 @@ contract SupplyChain {
 
     function totalSurveys() public view returns (uint){
         return surveys.length;
+    }   
+
+    function newActor(string _companyName, string _contactInfo, string _products) public returns (uint actorID){
+        actorID = actors.length++;
+        Actor storage a = actors[actorID];
+        a.companyName = _companyName;
+        a.contactInfo = _contactInfo;
+        a.products = _products;
+        return actorID;
+    }
+
+    function totalActors() public view returns (uint){
+        return actors.length;
     }   
 
 }
